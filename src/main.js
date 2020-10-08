4722cdcb458e22fdada50203282ad402c42c1221
单炜佳 <shanweijia@jiaaocap.com>
2020-10-08 23:50:31 +0800

    init(): 初始化代码

--- src/main.js
+++ src/main.js
@@ -3,6 +3,35 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+import dataV from '@jiaminghi/data-view'
+import VueRouter from 'vue-router'
+import routers from './route'
+import axios from 'axios'
+import './assets/common.less'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: routers
+});
+Vue.use(dataV)
+Vue.use(VueRouter);
+
+router.beforeEach((to, from, next) => {
+    document.title = to.name;
+    return next()
+})
+// import config from './config'
+
+// axios.defaults.baseURL = config.odoo_path;
+axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['Content-Type'] = 'application/json';
+axios.defaults.withCredentials = true;
+Vue.prototype.$axios = axios;
+
+Vue.config.productionTip = false
+
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
+    router,
+    axios
 }).$mount('#app')
